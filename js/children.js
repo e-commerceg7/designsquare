61c1a47188ab3ba0c3e239542adf34e6520dac61
hui zheng <huizh365@gmail.com>
2024-12-13 14:44:10 +0100

    EC-12 Modified filterAndDisplay function to accept two categories. Added funtion to category-buttons on Children's page

--- js/children.js
+++ js/children.js
@@ -1,2 +1,36 @@
 import { getProducts, productList, displayProducts, filterAndDisplay} from "./fetchProductData.js";
 filterAndDisplay("children");
+
+const jackets = document.getElementById('jackets')
+const jeans = document.getElementById('jeans')
+const hoodies = document.getElementById('hoodies')
+const shoes = document.getElementById('shoes')
+const pants = document.getElementById('pants')
+const sweatshirts = document.getElementById('sweatshirts')
+const shorts = document.getElementById('shorts')
+const sleepwear = document.getElementById('sleepwear')
+
+jackets.addEventListener("click", ()=>{
+    filterAndDisplay('children', 'jackets')
+})
+jeans.addEventListener("click", ()=>{
+    filterAndDisplay('children', 'jeans')
+})
+hoodies.addEventListener("click", ()=>{
+    filterAndDisplay('children', 'hoodies')
+})
+shoes.addEventListener("click", ()=>{
+    filterAndDisplay('children', 'shoes')
+})
+pants.addEventListener("click", ()=>{
+    filterAndDisplay('children', 'pants')
+})
+sweatshirts.addEventListener("click", ()=>{
+    filterAndDisplay('children', 'sweatshirts')
+})
+shorts.addEventListener("click", ()=>{
+    filterAndDisplay('children', 'shorts')
+})
+sleepwear.addEventListener("click", ()=>{
+    filterAndDisplay('children', 'sleepwear')
+})
