e1a3e1a3d6d0026127afdaaaa8c2edfcfd9cd22b
hui zheng <huizh365@gmail.com>
2024-12-13 15:12:58 +0100

    EC-12 Added category-filter funtion to men's page

--- js/children.js
+++ js/children.js
@@ -1,36 +1,55 @@
 import { getProducts, productList, displayProducts, filterAndDisplay} from "./fetchProductData.js";
 filterAndDisplay("children");
 
-const jackets = document.getElementById('jackets')
-const jeans = document.getElementById('jeans')
-const hoodies = document.getElementById('hoodies')
-const shoes = document.getElementById('shoes')
-const pants = document.getElementById('pants')
-const sweatshirts = document.getElementById('sweatshirts')
-const shorts = document.getElementById('shorts')
-const sleepwear = document.getElementById('sleepwear')
+const categoryMap = {
+    jackets: 'jackets',
+    jeans: 'jeans',
+    hoodies: 'hoodies',
+    shoes: 'shoes',
+    pants: 'pants',
+    sweatshirts: 'sweatshirts',
+    shorts: 'shorts',
+    sleepwear: 'sleepwear'
+}
 
-jackets.addEventListener("click", ()=>{
-    filterAndDisplay('children', 'jackets')
+Object.keys(categoryMap).forEach(id => {
+    const button = document.getElementById(id);
+    button.addEventListener("click", ()=>{
+        filterAndDisplay('children', categoryMap[id])
+    })
 })
-jeans.addEventListener("click", ()=>{
-    filterAndDisplay('children', 'jeans')
-})
-hoodies.addEventListener("click", ()=>{
-    filterAndDisplay('children', 'hoodies')
-})
-shoes.addEventListener("click", ()=>{
-    filterAndDisplay('children', 'shoes')
-})
-pants.addEventListener("click", ()=>{
-    filterAndDisplay('children', 'pants')
-})
-sweatshirts.addEventListener("click", ()=>{
-    filterAndDisplay('children', 'sweatshirts')
-})
-shorts.addEventListener("click", ()=>{
-    filterAndDisplay('children', 'shorts')
-})
-sleepwear.addEventListener("click", ()=>{
-    filterAndDisplay('children', 'sleepwear')
-})
+
+
+// const jackets = document.getElementById('jackets')
+// const jeans = document.getElementById('jeans')
+// const hoodies = document.getElementById('hoodies')
+// const shoes = document.getElementById('shoes')
+// const pants = document.getElementById('pants')
+// const sweatshirts = document.getElementById('sweatshirts')
+// const shorts = document.getElementById('shorts')
+// const sleepwear = document.getElementById('sleepwear')
+
+// jackets.addEventListener("click", ()=>{
+//     filterAndDisplay('children', 'jackets')
+// })
+// jeans.addEventListener("click", ()=>{
+//     filterAndDisplay('children', 'jeans')
+// })
+// hoodies.addEventListener("click", ()=>{
+//     filterAndDisplay('children', 'hoodies')
+// })
+// shoes.addEventListener("click", ()=>{
+//     filterAndDisplay('children', 'shoes')
+// })
+// pants.addEventListener("click", ()=>{
+//     filterAndDisplay('children', 'pants')
+// })
+// sweatshirts.addEventListener("click", ()=>{
+//     filterAndDisplay('children', 'sweatshirts')
+// })
+// shorts.addEventListener("click", ()=>{
+//     filterAndDisplay('children', 'shorts')
+// })
+// sleepwear.addEventListener("click", ()=>{
+//     filterAndDisplay('children', 'sleepwear')
+// })
