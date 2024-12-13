7b2ed51f8dcf26cb64006c3989fdcd776e2fbc7d
NINA HELDENIUS <nina.heldenius@gmail.com>
2024-12-13 12:42:54 +0100

    EC-79 Footer component done. Links and script in both files as comments.

---
+++ js/footer.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const footerElement = document.getElementById('footer');
+
+    fetch('footer.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to load footer: ' + response.status);
+            }
+            return response.text();
+        })
+        .then(data => {
+            console.log(data);
+            footerElement.innerHTML = data;
+        })
+        .catch(error => {
+            console.error(error);
+            footerElement.innerHTML = "<p>Error loading footer.</p>";
+        });
+});
+
+/*
+Put in head:
+<link rel="stylesheet" href="css/footer.css">
+
+Put in bottom of body:
+<footer id="footer"></footer>
+<script src="js/footer.js"></script>
+*/
