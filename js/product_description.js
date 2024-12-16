5cdbaa53d5a5e0c72582e81f51048a2b2de697c6
Annika Karlsson <en_anka@hotmail.com>
2024-12-16 14:12:35 +0000

    EC-27 adding functionality with API endpoint searchById

--- js/product_description.js
+++ js/product_description.js
@@ -1,48 +1,40 @@
 
-const temporaryId = "675874568c163b7fe8d0b275" //A static ID taken from the 
-// existing database, awaitng functionallity to send the ID from the product page list
+const urlParams = new URLSearchParams(window.location.Search); //get URL sent from productlist page
+const productId = "675874568c163b7fe8d0b275" /* urlParams.get("id"); */ //Extract the product id
+/* const currentProduct = getProduct(productId);  */
 
-const testItem = { /// manual extract from the database used for testing functions
-    "categories": [
-        "jeans",
-        "men"
-    ],
-    "_id": "675874568c163b7fe8d0b275",
-    "name": "Jeans",
-    "description": "Comfortable and stylish jeans.",
-    "price": {
-        "$numberDecimal": "499"
-    },
-    "color": "Blue",
-    "stock": 30,
-    "date": "2024-12-10T17:03:18.900Z",
-    "__v": 0,
-    "image": "https://image.hm.com/assets/hm/bb/56/bb56b5102fef5008cc73e550fbae6b27d8ead12e.jpg?imwidth=1536"
+loadPage(productId) //add dynamic content to page
+
+async function loadPage(productId){
+    const product = await getProduct(productId);
+    displayProduct(product);
+
+    //Add the function to add the item to the cart on the Add to cart button
+    const addToCartBtn = document.querySelector(".to-cart-btn");
+    addToCartBtn.addEventListener("click", () => {
+        shoppingCart.addToCart(product);
+    });
+    
 }
 
-function displayProduct(product){
 
+function displayProduct(product){
+    const productData = product;
     const productDetails = document.getElementById("product-description");
     const itemImage = document.querySelector("img");
     
-    itemImage.src = product.image;
+    //Display the current products details on the page
+    itemImage.src = productData.image;
     productDetails.innerHTML = `
         <article id="product-details">
             <section id="description-header">
-                <h1 id="item-name">${product.name}</h1>
-                <h2 id="color">${product.color}</h2>
-                <h2 id="price">${product.price.$numberDecimal} kr</h2>
-                <p id="description">${product.description}</p>
+                <h1 id="item-name">${productData.name}</h1>
+                <h2 id="color">${productData.color}</h2>
+                <h2 id="price">${productData.price.$numberDecimal} kr</h2>
+                <p id="description">${productData.description}</p>
                 </section>
     `    
 };
-
-const addToCartBtn = document.querySelector(".to-cart-btn");
-addToCartBtn.addEventListener("click", () => {
-shoppingCart.addToCart(testItem);
-});
-
-displayProduct(testItem);
 
 //Object with the different functions for the shopping cart
 const shoppingCart = {
@@ -51,19 +43,21 @@ const shoppingCart = {
         
             const itemIndex = customerCart.findIndex(item => item.id === productId && item.size === document.getElementById("size").value);
             
+            console.log(productId)
             return itemIndex;
     },
-    addToCart: function (product){
+    addToCart: async function (currentProduct){
         const customerCart = JSON.parse(localStorage.getItem(this.name)) || [];
-        const inCartIndex = this.checkIfInCart(customerCart, testItem._id);
-        
+        const inCartIndex = this.checkIfInCart(customerCart, currentProduct._id);
+        const productData = await currentProduct;
         if(inCartIndex !== -1){
             customerCart[inCartIndex].quantity += 1;
         } else {
             const item = {
-                id: product._id,
+                id: productData._id,
                 size: document.getElementById("size").value,
-                price: product.price,
+                color: productData.color,
+                price: productData.price.$numberDecimal,
                 quantity: 1
             };
             customerCart.push(item);
@@ -74,18 +68,17 @@ const shoppingCart = {
 };
 
 
-//Fetch a single product by product ID, might not be neccessary if an object is returned from productlist page
+//Fetch a single product by productId
 async function getProduct(productId){
     try{
-        const response = await fetch("https://ecommerce-api-seven-omega.vercel.app/products/" + productId);
+        const response = await fetch("https://ecommerce-api-ashy-ten.vercel.app/products/" + productId);
     
         if(!response.ok) {
             throw new Error("Det gick inte att hämta produkten.");
         }
     
-        const product = await response.json();
-        console.log(product);
-        return product;
+        const result = await response.json();
+        return result;
 
     } catch(error) {
         console.error("Fel vid hämtning av produkten", error);
