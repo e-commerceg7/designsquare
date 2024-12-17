8412212664b22f8833a2de70d3b49af12a65e2e7
Annika Karlsson <en_anka@hotmail.com>
2024-12-17 09:16:46 +0000

    Bugfix/ Adding missing files

--- js/children.js
+++ js/children.js
@@ -28,36 +28,4 @@ Object.keys(categoryMap).forEach(id => {
 })
 
 
-// const jackets = document.getElementById('jackets')
-// const jeans = document.getElementById('jeans')
-// const hoodies = document.getElementById('hoodies')
-// const shoes = document.getElementById('shoes')
-// const pants = document.getElementById('pants')
-// const sweatshirts = document.getElementById('sweatshirts')
-// const shorts = document.getElementById('shorts')
-// const sleepwear = document.getElementById('sleepwear')
 
-// jackets.addEventListener("click", ()=>{
-//     filterAndDisplay('children', 'jackets')
-// })
-// jeans.addEventListener("click", ()=>{
-//     filterAndDisplay('children', 'jeans')
-// })
-// hoodies.addEventListener("click", ()=>{
-//     filterAndDisplay('children', 'hoodies')
-// })
-// shoes.addEventListener("click", ()=>{
-//     filterAndDisplay('children', 'shoes')
-// })
-// pants.addEventListener("click", ()=>{
-//     filterAndDisplay('children', 'pants')
-// })
-// sweatshirts.addEventListener("click", ()=>{
-//     filterAndDisplay('children', 'sweatshirts')
-// })
-// shorts.addEventListener("click", ()=>{
-//     filterAndDisplay('children', 'shorts')
-// })
-// sleepwear.addEventListener("click", ()=>{
-//     filterAndDisplay('children', 'sleepwear')
-// })
