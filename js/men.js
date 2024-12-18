1d5f3f86729e3ffef076151da0d871d9d51f5b34
hui zheng <huizh365@gmail.com>
2024-12-18 08:55:24 +0100

    EC-98 small changes: clean code

--- js/men.js
+++ js/men.js
@@ -1,35 +1,3 @@
-// import { getProducts, productList, displayProducts, filterAndDisplay, filteredProducts, sortProducts} from "./fetchProductData.js";
-// filterAndDisplay("men");
-
-// const sortDropdown = document.getElementById("sort-products")
-// sortDropdown.addEventListener("change", (event)=>{
-//     const sortBy = event.target.value
-//     const sortedProducts = sortProducts(sortBy)
-//     displayProducts(sortedProducts)
-// })
-
-
-// const categoryMap = {
-//     men: 'men',
-//     jeans: 'jeans',
-//     shorts: 'shorts',
-//     polo: 'polo',
-//     shirts: 'shirts',
-//     "t-shirt": 't-shirt',
-//     pants: 'pants'
-// }
-
-// Object.keys(categoryMap).forEach(id => {
-//     const button = document.getElementById(id);
-//     if (!button) {
-//         console.error(`Button with id "${id}" not found in the HTML.`);
-//         return;
-//     }
-//     button.addEventListener("click", ()=>{
-//         filterAndDisplay('men', categoryMap[id])
-//     })
-// })
-
 import { getProducts, productList, displayProducts, 
     filterAndDisplay, filteredProducts, sortProducts, 
     filterProducts, loadingFilter, getSubcategories, 
